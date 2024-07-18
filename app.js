6e01d8087222b81b3e5442b03c6bab68999e2e97
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-07-18 13:20:25 +0800

    Use Express to serve website

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-const http = require("http");
-const fs = require("fs");
 const express = require("express");
-
-const port = 8080;
 const app = express();
+const path = require("path");
+
+const port = process.env.PORT || 8080;
 
 app.use(express.static("./"));
 
-http.createServer((req, res) => {
-    let readStream;
-    if (req.url === "/styles.css") {
-        res.writeHead(200, { "Content-Type": "text/css" });
-        readStream = fs.createReadStream("styles.css");
-    } else {
-        const validPages = ["/", "/index.html", "/about.html", "/contact.html"];
-        const statusCode = validPages.includes(req.url) ? 200 : 404;
-        res.writeHead(statusCode, { "Content-Type": "text/html" });
+const paths = ["/", "*", "/index", "/about", "/contact"];
 
-        readStream = fs.createReadStream(
-            statusCode === 200
-                ? req.url.slice(1)
-                    ? req.url.slice(1)
-                    : "index.html"
-                : "404.html",
-        );
-    }
-    readStream.pipe(res);
-}).listen(port);
-console.log(`Server is listening on port ${port}`);
+paths.forEach( currPath => app.get(currPath, (req, res) => {
+    const filename = (currPath === "/") 
+        ? "/index.html" 
+        : (currPath === "*") 
+            ? "/404.html" 
+            : `${currPath}.html`;
+    res.sendFile(path.join(__dirname, filename));
+}));
+
+app.listen(port)
+console.log(`Server started at http://localhost:${port}`);
